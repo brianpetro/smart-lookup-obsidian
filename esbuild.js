c4ed6c4e4be9c553c7cf9509da81d03985be7086
Brian Joseph Petro <brian@smartconnections.app>
2026-05-30 08:50:09 -0400

    release process

--- esbuild.js
+++ esbuild.js
@@ -1,23 +1,22 @@
 import esbuild from 'esbuild';
 import path from 'path';
 import { build_plugin } from 'obsidian-smart-env/build/build_plugin.js';
+import { build_smart_env_config } from 'obsidian-smart-env/build/build_env_config.js';
 
-/**
- * Create banner comment for esbuild bundle.
- * @param {{name: string, version: string, author: string}} pkg package metadata
- * @returns {string} banner comment
- */
-function create_banner(pkg) {
-  const year = new Date().getFullYear();
-  return `/*! ${pkg.name} v${pkg.version} | (c) ${year} ${pkg.author} */`;
-}
+const roots = [
+  path.resolve(process.cwd(), 'src'),
+];
 
 build_plugin({
   esbuild,
-  build_banner: create_banner,
   entry_point: 'src/main.js',
   entry_point_from_argv: true,
+  env_config_builder: build_smart_env_config,
+  env_config_output_dir: process.cwd(),
+  env_config_roots: roots,
   external: [
+    '@codemirror/state',
+    '@codemirror/view',
     '@xenova/transformers',
     '@huggingface/transformers',
     'http',
@@ -29,3 +28,4 @@ build_plugin({
   console.error('Error in build process:', err);
   process.exit(1);
 });
+
